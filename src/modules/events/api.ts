ebee988c34bca9bb4c4c36c1a1b609c3177bcca6
Dawid Kobierski <dkobierski@objectivity.co.uk>
2020-02-20 10:14:34 +0100

    Add search events and clickable logo

--- src/modules/events/api.ts
+++ src/modules/events/api.ts
@@ -5,7 +5,10 @@ import { ApiResponse } from '../../models/apiResponse';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
-export const fetchEvents = () => axios.get<ApiResponse<Event[]>>('events');
+export const fetchEvents = (searchText?: string) => {
+  const params = searchText ? { search: searchText } : undefined;
+  return axios.get<ApiResponse<Event[]>>('events', { params });
+};
 export const fetchEvent = (eventId: string) => axios.get<ApiResponse<Event>>(`events/${eventId}`);
 export const createEvent = (event: Event) => axios.post<ApiResponse<Event>>('events', event);
 export const deleteEvent = (eventId: string) => axios.delete<ApiResponse<Event>>(`events/${eventId}`);
--- src/modules/events/pages/EventList.tsx
+++ src/modules/events/pages/EventList.tsx
@@ -5,25 +5,37 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import LinkAsButton from '../../../components/buttons/LinkAsButton';
 import EventItem from '../components/EventItem';
+import useDebounce from '../../../hooks/useDebounce';
 import { Event } from '../models/event';
 import { fetchEvents } from '../api';
 
 const EventList: React.FC<RouteComponentProps> = () => {
   const { t } = useTranslation('events');
   const [events, setEvents] = useState<Event[]>([]);
+  const [searchText, setSearchText] = useState<string>('');
+  const debouncedSearchText = useDebounce(searchText, 500);
 
   useEffect(() => {
-    fetchEvents().then(response => setEvents(response.data.data));
-  }, []);
+    fetchEvents(debouncedSearchText).then(response => setEvents(response.data.data));
+  }, [debouncedSearchText]);
 
   return (
     <div className="w-full">
       <div className="flex justify-between">
         <h1 className="font-bold text-xl text-gray-800">{t('events')}</h1>
-        <LinkAsButton to="create">
-          <FontAwesomeIcon icon="plus" className="mr-2" />
-          {t('createEvent')}
-        </LinkAsButton>
+        <div className="flex justify-between">
+          <input
+            className="shadow appearance-none border rounded block mr-2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="Search..."
+            value={searchText}
+            onChange={e => setSearchText(e.currentTarget.value)}
+          />
+          <LinkAsButton to="create">
+            <FontAwesomeIcon icon="plus" className="mr-2" />
+            {t('createEvent')}
+          </LinkAsButton>
+        </div>
       </div>
       <div className="flex flex-wrap -mx-3">
         {events.map(event => (
